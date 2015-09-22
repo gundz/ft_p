f95513e67123a7224a34e4a7e1edd96f16bd65ac
gundz <fgundlac@student.42.fr>
2015-09-22 23:18:05 +0200

    added usage and options

--- client_srcs/srcs/main.c
+++ client_srcs/srcs/main.c
@@ -1,20 +1,28 @@
+#include <libft.h>
 #include <libftsocket.h>
 
-int
-main(void)
-{	
-	int					portno = 5001;
+int						main_client(char *addr, const int portno)
+{
 	t_socket			serv;
 
-	if (new_socket(&serv, AF_INET, inet_addr("127.0.0.1"), htons(portno)) == -1)
+	if (new_socket(&serv, AF_INET, inet_addr(addr), htons(portno)) == -1)
 		return (EXIT_FAILURE);
 
 	if (connect(serv.fd, (struct sockaddr *)&serv.addr, serv.socklen) < 0)
 		perror("Error connecting 1");
 
-	send_msg_type(serv.fd, UINT32);
+	send_msg_type(serv.fd, MSG);
 
 	close(serv.fd);
+	return (EXIT_SUCCESS);
+}
 
-	return (0);
+int						main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: ./client addr port\n");
+		return (EXIT_FAILURE);
+	}
+	return (main_client(argv[1], ft_atoi(argv[2])));
 }
--- server_srcs/srcs/main.c
+++ server_srcs/srcs/main.c
@@ -1,9 +1,8 @@
+#include <libft.h>
 #include <libftsocket.h>
 
-int
-main(void)
+int						main_server(const int portno)
 {
-	int					portno = 5001;
 	t_socket			serv;
 	t_socket			cli;
 
@@ -39,6 +38,15 @@ main(void)
 	printf("%s: disconnected\n", inet_ntoa(cli.addr.sin_addr));
 
 	close(serv.fd);
+	return (EXIT_SUCCESS);	
+}
 
-	return (0);
+int						main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage: ./server port\n");
+		return (EXIT_FAILURE);
+	}
+	return (main_server(ft_atoi(argv[1])));
 }
